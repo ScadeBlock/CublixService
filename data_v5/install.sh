23645ce617a3df51efe256407381f13f8b4a8d8f
D <89845150+ScadeBlock@users.noreply.github.com>
2024-08-26 14:32:26 +0700

    Update install.sh

--- data_v5/install.sh
+++ data_v5/install.sh
@@ -1,1 +1,1 @@
-clear && echo "[Cublix 5.8] Đang cài đặt..." && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v3dir/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/ScadeBlock/CublixService/raw/main/data_v5/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login

094809ce4ecaf6371cb80a9d187b60003a4d538d
D <89845150+ScadeBlock@users.noreply.github.com>
2024-08-28 21:06:17 +0700

    Create install.sh

---
+++ data_v5/neoqc/install.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/ScadeBlock/CublixService/raw/main/data_v5/neoqc/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
